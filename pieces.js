5cb903cfd41548b061a2d80cd25b42fc70c93ad2
ThierryCode <kamamyandry@yahoo.fr>
2023-12-14 15:38:34 +0300

    Première affichage de tout les produit avec les ficheir js et css

--- pieces.js
+++ pieces.js
@@ -2,32 +2,36 @@
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
-//Création des 4 balises
-const article = pieces[1];
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
+for (let i = 0; i < pieces.length; i++) {
 
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
+    const article = pieces[i];
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d’une balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+    // Création des balises 
+    const imageElement = document.createElement("img");
+    imageElement.src = article.image;
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = article.nom;
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+    const stockElement = document.createElement("p");
+    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+    
+    // On rattache la balise article a la section Fiches
+    sectionFiches.appendChild(pieceElement);
+    // On rattache l’image à pieceElement (la balise article)
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    //Ajout des éléments au DOM pour l'exercice
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(stockElement);
 
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
-
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "Aucune catégorie";
-
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? "Pas de desctription pour le moment";
-
-const stockElement = document.createElement("p");
-stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
-
-//Ajout des 4 balises dans une div class .fiche parent 
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(stockElement);
-
+ }
